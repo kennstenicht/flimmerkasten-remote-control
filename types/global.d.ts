843652142ecb75f51697b33745f14ae9383bdd09
Christoph Wiedenmann <christoph@ag-prop.com>
2023-10-29 14:16:36 +0100

    Fix(Types): Fix types errors

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,2 +1,12 @@
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
+
+declare module '*.css' {
+  const styles: { [className: string]: string };
+  export default styles;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
