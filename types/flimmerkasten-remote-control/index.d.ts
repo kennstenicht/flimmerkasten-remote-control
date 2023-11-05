54865e660b74a3aba1f612ab10f331f53f4f635f
Christoph Wiedenmann <christoph@ag-prop.com>
2023-11-05 22:36:46 +0100

    Fix(Types): Fix css and svg types

--- types/flimmerkasten-remote-control/index.d.ts
+++ types/flimmerkasten-remote-control/index.d.ts
@@ -1,1 +1,3 @@
 import 'ember-source/types';
+import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,3 @@
-import '@glint/environment-ember-loose';
-import '@glint/environment-ember-template-imports';
-
 declare module '*.css' {
   const styles: { [className: string]: string };
   export default styles;
